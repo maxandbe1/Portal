72d8fcae2d787196e337e3e207970974e41e6d65
maxandbe1 <maxandbe1@proton.me>
2026-05-24 22:49:11 -0400

    Update portal-runtime.js

--- portal-runtime.js
+++ portal-runtime.js
@@ -1,4 +1,17 @@
-
 import { EventBus } from "./event-bus.js";
 import { initSession } from "../population/session-init.js";
 import { loadModules } from "./module-loader.js";
+
+export function bootPortal() {
+  window.Portal = {
+    session: initSession(),
+    bus: new EventBus(),
+    modules: {},
+    engines: {}
+  };
+
+  console.log("%cPortal Runtime Booted", "color:#0f0;font-weight:bold;");
+  console.log("Session:", window.Portal.session);
+
+  loadModules();
+}
