82bece3511471413380bf87b72871e4fda71d89f
maxandbe1 <maxandbe1@proton.me>
2026-05-26 14:52:08 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { bootPortal } from "./runtime/portal-runtime.js";
 
-
-// Start the Portal runtime (session, event bus, engines, modules)
-bootPortal();
-
-// Mount the React application
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
