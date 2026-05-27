d5d71facf3e9d3b4db027a8205b58b7549eb04ba
maxandbe1 <maxandbe1@proton.me>
2026-05-27 13:31:12 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { loadPortalModules } from "./runtime/module-loader.js";
+
+loadPortalModules();
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
